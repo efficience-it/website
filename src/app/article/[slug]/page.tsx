891371dee5e32d4e3d1cc59bbc0a2c5fff581bd7
Louis-Arnaud <la.catoire@gmail.com>
2026-03-12 09:53:15 +0100

    feat: ajouter SpeakableSpecification au schema des articles (#438)
    Indique aux IA (Google AI Overviews, assistants vocaux) quelles
    parties extraire : le h1 et le premier paragraphe.
    
    Closes #389

--- src/app/article/[slug]/page.tsx
+++ src/app/article/[slug]/page.tsx
@@ -106,6 +106,10 @@ export default async function ArticlePage({ params }: ArticlePageProps) {
     url,
     wordCount: post.wordCount,
     inLanguage: "fr-FR",
+    speakable: {
+      "@type": "SpeakableSpecification",
+      cssSelector: ["h1", "article > p:first-of-type"],
+    },
     ...(isTech && { proficiencyLevel: post.proficiencyLevel ?? "Intermediate" }),
   };
 
