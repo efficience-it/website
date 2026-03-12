e8f96a150e0918dc9e80029b5e634365aba9ca7d
lacatoire <la.catoire@gmail.com>
2026-03-12 20:00:54 +0100

    fix: mock IntersectionObserver dans jest.setup pour jsdom

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,21 @@
 import "@testing-library/jest-dom";
+
+global.IntersectionObserver = class IntersectionObserver {
+  readonly root: Element | null = null;
+  readonly rootMargin: string = "";
+  readonly thresholds: ReadonlyArray<number> = [];
+  constructor(private callback: IntersectionObserverCallback) {
+    setTimeout(() => {
+      this.callback(
+        [{ isIntersecting: true } as IntersectionObserverEntry],
+        this as unknown as IntersectionObserver,
+      );
+    }, 0);
+  }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  takeRecords(): IntersectionObserverEntry[] {
+    return [];
+  }
+};
