7da68e0892a898e89015d609b0530ef372dbeec9
lacatoire <la.catoire@gmail.com>
2026-03-12 09:02:06 +0100

    feat: ajouter les directives robots.txt pour les bots IA
    GPTBot, OAI-SearchBot, ClaudeBot, PerplexityBot, Googlebot-Extended
    et meta-externalagent sont explicitement autorisés.
    
    Closes #385

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,23 @@ export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: [
+          "GPTBot",
+          "OAI-SearchBot",
+          "ClaudeBot",
+          "PerplexityBot",
+          "Googlebot-Extended",
+          "meta-externalagent",
+        ],
+        allow: "/",
+      },
+    ],
     sitemap: "https://www.itefficience.com/sitemap.xml",
   };
 }
