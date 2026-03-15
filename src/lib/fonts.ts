45e5775be4afc33c2d62aa32e8d3458ecda2ef82
Louis-Arnaud <la.catoire@gmail.com>
2026-03-15 22:40:30 +0100

    perf: restreindre les variantes de fonts a 400/500/600/700 (#519)

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -2,12 +2,14 @@ import { Montserrat, Exo } from "next/font/google";
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
   variable: "--font-montserrat",
   display: "swap",
 });
 
 export const exo = Exo({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
   variable: "--font-exo",
   display: "swap",
 });
