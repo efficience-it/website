4a8da896fb4bfe4a6d836c90ffa45dd3c73ab3cd
lacatoire <la.catoire@gmail.com>
2026-03-16 09:37:11 +0100

    chore: supprimer le code mort (Skeleton, jobCategories, teamValues)

--- data/team.ts
+++ data/team.ts
@@ -4,29 +4,6 @@ export interface TeamMember {
   photo: string;
 }
 
-export const teamValues = [
-  {
-    title: "Agilité",
-    description:
-      "La réactivité et la communication sont les maîtres mots de notre façon de travailler.",
-  },
-  {
-    title: "Expertise",
-    description:
-      "Membres de l'AFUP, nous effectuons une veille régulière sur les tendances web et les meilleures pratiques.",
-  },
-  {
-    title: "Cohésion d'équipe",
-    description:
-      "Le bien-être au travail est une valeur importante. Nous cultivons la convivialité et l'entraide.",
-  },
-  {
-    title: "Diversité",
-    description:
-      "Nous suivons la charte de diversité et maintenons une parité entre 40 et 60 %.",
-  },
-];
-
 export const teamMembers: TeamMember[] = [
   {
     name: "Louis-Arnaud Catoire",
