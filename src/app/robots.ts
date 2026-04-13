119e288352f23d9f0925a11ae4c48e529b7b4119
Luc <luc.rousseaupro@gmail.com>
2026-04-13 16:05:57 +0200

    robots.txt block domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,10 +4,13 @@ export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/domain/"],
+      },
+    ],
     sitemap: "https://www.itefficience.com/sitemap.xml",
   };
 }
